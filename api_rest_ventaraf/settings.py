1edc21a506826013cdf261bbf971f290b324f66c
Lucas Zurverra <lzurverra@gmail.com>
2021-05-23 20:29:36 -0300

    instalacion de app anuncios

--- api_rest_ventaraf/settings.py
+++ api_rest_ventaraf/settings.py
@@ -1,10 +1,10 @@
 import os
-import dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = os.path.join(BASE_DIR, "api_rest_ventaraf")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'anuncios',
+    'anuncios.apps.AnunciosConfig',
     'rest_framework',
 ]
 
