ab51fa352537834e3952c757e4714d542478a4f2
Lucas Zurverra <lzurverra@gmail.com>
2021-05-23 20:56:57 -0300

    vista de ejemplo para token

--- anuncios/views.py
+++ anuncios/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.authtoken import Token
+from dotenv import load_dotenv
+from api_rest_ventaraf.settings import BASE_DIR, LOAD_ENV
 
-# Create your views here.
+@api_view(['GET'])
+def login(request):
+    bd_user = os.environ['BD_USER']
+    user = User.objects.get(username=bd_user)
+    token = Token.objects.create(user=user)
