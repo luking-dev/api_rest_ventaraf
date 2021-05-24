9433b57336294a10323f127199fe1f15a29367c7
Lucas Zurverra <lzurverra@gmail.com>
2021-05-24 14:28:57 -0300

    campo imagen no requerido

--- anuncios/models.py
+++ anuncios/models.py
@@ -5,12 +5,12 @@ class Anuncio(models.Model):
     nombre = models.CharField(null=False, blank=False, max_length=40)
     imagen = models.URLField(null=True, blank=True, default=None)
     direccion = models.CharField(null=False, blank=False, max_length=40)
-    latitud = models.CharField(null=False, blank=False, max_length=20)
-    longitud = models.CharField(null=False, blank=False, max_length=20)
+    latitud = models.CharField(null=True, blank=True, max_length=20)
+    longitud = models.CharField(null=True, blank=True, max_length=20)
     titulo = models.CharField(null=False, blank=False, max_length=50)
     precio = models.DecimalField(null=False, blank=False, max_digits=5, decimal_places=2)
     descripcion = models.TextField(null=True, blank=True, max_length=500, default=None)
-    email = models.EmailField(null=False, blank=False)
+    email = models.EmailField(null=True, blank=True)
     celular = models.CharField(null=True, blank=True, max_length=20, default=None)
 
     def __str__(self):
